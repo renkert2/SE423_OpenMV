93d4790cea69a09ef6fa17783891654819cd4178
phil_renkert <renkert2@illinois.edu>
2022-03-22 21:48:39 -0500

    Bug fixes

--- MultiBlob_V4.py
+++ MultiBlob_V4.py
@@ -14,14 +14,14 @@ uart = pyb.UART(3)
 uart.init(115200, bits=8, parity=None)
 
 # Packets to Send
-blob_packet = '<fff'
+blob_packet = 'fff'
 
 # Setup RED LED for easier debugging
 red_led   = pyb.LED(1)
 green_led = pyb.LED(2)
 blue_led  = pyb.LED(3)
 
-def snapshot(data):
+def snapshot():
     green_led.on()
 
     img = sensor.snapshot()
@@ -36,26 +36,20 @@ def snapshot(data):
 
     msg = "**".encode()
     uart.write(msg)
-    float_bytes = []
     for b in blob_largest:
         img.draw_rectangle(b.rect(), color = (255, 0, 0))
         img.draw_cross(b.cx(), b.cy(), color = (0, 255, 0))
 
-        a = float(b.area())
-        x_cnt = float(b.cx())
-        y_cnt = float(b.cy())
+        a = b.area()
+        x_cnt = b.cx()
+        y_cnt = b.cy()
 
         # Send the blob area and centroids over UART
         b = ustruct.pack(blob_packet, a, x_cnt, y_cnt)
         uart.write(b)
 
-        float_bytes.append(b)
-
     img.compress(quality=20) # Compress in place
     green_led.off()
-
-    float_bytes_concat = b''.join(float_bytes)
-    return float_bytes_concat
 
 def jpeg_image_size(data):
     blue_led.on()
@@ -73,8 +67,9 @@ def jpeg_image_read(data):
     return bytes()
 
 # Register call backs.
-interface.register_callback(snapshot)
+interface.setup_loop_callback(snapshot)
 interface.register_callback(jpeg_image_size)
 interface.register_callback(jpeg_image_read)
 
+sensor.snapshot() # Make sure we start with something in the frame buffer
 interface.loop(recv_timeout=5000, send_timeout=5000)
